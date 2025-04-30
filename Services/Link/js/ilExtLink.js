ca6ee7514aacb503fb055a25e7425efcae2d50fd
jheim2 <johannes.heim@rz.uni-freiburg.de>
2025-04-30 14:19:32 +0200

    Open external links with `window=current` in the same tab; all others still open in a new tab. Add `target` param from current URL if available.

--- Services/Link/js/ilExtLink.js
+++ Services/Link/js/ilExtLink.js
@@ -21,3 +21,39 @@ il.ExtLink = {
 		$(selector).find("a.linkified[target='_blank']").attr("rel", "noreferrer noopener");
 	}
 }
+
+// Open external links with `window=current` in the same tab; all others still open in a new tab. Add `target` param from current URL if available.
+
+document.addEventListener("DOMContentLoaded", function() {
+    const links = document.querySelectorAll('a[target="_blank"]');
+
+    // Get the "target" parameter from the current page URL
+    const currentUrlParams = new URLSearchParams(window.location.search);
+        //  console.log(currentUrlParams);
+    const targetParam = currentUrlParams.get("target");
+        //  console.log(targetParam);
+
+    links.forEach(link => {
+      const url = new URL(link.href);
+          //  console.log(url.hostname);
+          //  console.log(url);
+
+      if (
+        url.hostname === window.location.hostname &&
+        url.searchParams.get("window") === "current"
+      ) {
+        link.target = '_self';
+        // console.log(`Changing ${link.href} to target _self`);
+
+        // If the current page has a "target" param, add it to the link
+        if (targetParam !== null) {
+          url.searchParams.set("target", targetParam);
+        // Remove window=same
+          url.searchParams.delete("window");
+          link.href = url.toString();
+          // console.log(`Updated link with target param: ${link.href}`);
+        }
+      }
+    });
+  });
+
